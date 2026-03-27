03624aa11f3f0afd145c1490a8e6a4da8634d2ee
Life <simoleo89@gmail.com>
2026-03-27 17:48:35 +0100

    feat: show hidden catalog nodes in admin mode

--- src/components/catalog/views/navigation/CatalogNavigationSetView.tsx
+++ src/components/catalog/views/navigation/CatalogNavigationSetView.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { ICatalogNode } from '../../../../api';
+import { useCatalogAdmin } from '../../CatalogAdminContext';
 import { CatalogNavigationItemView } from './CatalogNavigationItemView';
 
 export interface CatalogNavigationSetViewProps
@@ -11,12 +12,14 @@ export interface CatalogNavigationSetViewProps
 export const CatalogNavigationSetView: FC<CatalogNavigationSetViewProps> = props =>
 {
     const { node = null, child = false } = props;
+    const catalogAdmin = useCatalogAdmin();
+    const adminMode = catalogAdmin?.adminMode ?? false;
 
     return (
         <>
             { node && (node.children.length > 0) && node.children.map((n, index) =>
             {
-                if(!n.isVisible) return null;
+                if(!adminMode && !n.isVisible) return null;
 
                 return <CatalogNavigationItemView key={ index } child={ child } node={ n } />;
             }) }
